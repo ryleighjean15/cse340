3091307f00f6211c7bf70875575bf65e1a5c9f3f
ryleigh rowell <ryleighjean15@gmail.com>
2024-11-22 13:51:20 -0700

    updates

--- routes/static.js
+++ routes/static.js
@@ -3,14 +3,10 @@ const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-// router.use(express.static("public"));
-// router.use("/css", express.static(__dirname + "public/css"));
-// router.use("/js", express.static(__dirname + "public/js"));
-// router.use("/images", express.static(__dirname + "public/images"));
-
-router.get('/', (req, res) => {
-    res.send('Static route');
-  });
+router.use(express.static("public"));
+router.use("/css", express.static(__dirname + "public/css"));
+router.use("/js", express.static(__dirname + "public/js"));
+router.use("/images", express.static(__dirname + "public/images"));
 
 module.exports = router;
 
