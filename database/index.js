0826b297592fdd78dc2637ee9a55d17a13453b2b
ryleigh rowell <ryleighjean15@gmail.com>
2024-11-24 00:07:13 -0700

    url fixed

--- database/index.js
+++ database/index.js
@@ -32,6 +32,9 @@ module.exports = {
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+    },
   })
   module.exports = pool
 }
